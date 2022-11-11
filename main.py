f5d8f90360837e926190aeb4cbd22cf0c480465e
Timur Seyidov <seyidov@yandexlyceum.ru>
2022-11-11 13:07:55 +0500

    - Заменен логгер на loguru - Разделены логи по уровню - Изменена конфигурация

--- main.py
+++ main.py
@@ -1,48 +1,29 @@
-import sys, os
+import sys
 
-from datetime import datetime
 from dynaconf import Dynaconf
-from pathlib import PurePath, Path
-
 from utils.Logger import Logger
 from mysql.connector import connect, Error
 
 
 class Application:
     def __init__(self, config_file):
-        self.logger = Logger(__name__)
         self.config = Dynaconf(
             settings_file=[config_file],
             environments=False
         )
+        logger_config = self.config.get('Logger')
+        self.logger = Logger(logger_config)
         try:
-            self.__logger_config__(
-                self.config.get('Logger', dict())
-            )
             self.connect = self.__db_connect__(
                 self.config.get('MySQL', dict())
             )
         except Exception as e:
-            self.logger.critical('Error', exc_info=True)
+            self.logger.exception(e)
             sys.exit()
 
-    def __logger_config__(self, config=dict()):
-        curr = Path(__file__).parent.resolve()
-        path = config.get('path', None)
-        if path:
-            curr = PurePath(curr, path)
-        filename = config.get('filename', None)
-        if filename:
-            filename = datetime.now().strftime(filename)
-            filename = PurePath(curr, filename)
-            try:
-                os.makedirs(curr, mode=0o755, exist_ok=True)
-            except:
-                filename = None
-        self.logger.format_date = config.get('format_date', None)
-        self.logger.format_message = config.get('format_message', None)
-        self.logger.filename = filename
-    def __db_connect__(self, config=dict()):
+    def __db_connect__(self, config=None):
+        if not config:
+            raise Exception('Не найдена конфигурация базы данных')
         db_host = config.get('host')
         db_port = int(config.get('port'))
         db_user = config.get('username')
